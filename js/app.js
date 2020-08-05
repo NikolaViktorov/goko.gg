f82a33f0cf84cb2892e192ba5fbd7992d57c6685
Nikola Viktorov <40743601+NikolaViktorov@users.noreply.github.com>
2020-08-05 05:07:08 +0300

    Added Functionalities
    Now the match shows who won the game as well as displays the icon of your champion (More specific one do not work for now like Jarvan IV, Kai'sa, Nunu & Willump). Matches are symetrical now. When you press the sesarch button you now have to wait until its loaded to press again. Added automatic version checker to check for the most recent version of ddragon cdn.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { getChampionById, getUserBySummonerName, getIconById, getMatchesByEncAccId, getMatchById } from './data.js';
+import { setMostRecentVersion, getChampionById, getUserBySummonerName, getIconById, getMatchesByEncAccId, getMatchById } from './data.js';
 import el from './dom.js';
 
 window.addEventListener('load', () => {
@@ -10,14 +10,16 @@ window.addEventListener('load', () => {
     const loadingEl = document.getElementById('loading');
     const numberGamesInputEl = document.getElementById('numberGames');
     
-    document.getElementById('submit').addEventListener('click', renderData)
+    const submitBtn = document.getElementById('submit');
+    submitBtn.addEventListener('click', renderData)
     
     async function renderData(e) {
         e.preventDefault();
-        
+
         try {
             loadingEl.classList.remove('hidden');
             containerEl.innerHTML = '';
+            submitBtn.disabled = true;
             const data = await getUserBySummonerName(nameInputEl.value);
             const matches = await getMatchesByEncAccId(data.accountId);
 
@@ -33,25 +35,40 @@ window.addEventListener('load', () => {
                     data.champNames.push('Version for Champion Finder too old!')
                 }
 
-                const table = createTable(data.names, data.champNames, data.profileIconIds);
+                const table = createTable(data.names, data.champNames,
+                     data.profileIconIds, data.firstTeamState,
+                      data.secondTeamState);
                 containerEl.appendChild(table);
             }     
         } catch (error) {
             console.error(error);
         } finally {
             loadingEl.classList.add('hidden');
-            //nameInputEl.value = '';
-            //numberGamesInputEl.value = '';
+            submitBtn.disabled = false;
         }
     }
 });
 
-function createTable(names, champNames, profileIconIds) {
+function createTable(names, champNames, profileIconIds, stateFirstTeam, stateSecondTeam) {
     function getIconUrl(id) {
-        return `http://ddragon.leagueoflegends.com/cdn/9.3.1/img/profileicon/${id}.png`;
+        return `http://ddragon.leagueoflegends.com/cdn/10.15.1/img/profileicon/${id}.png`;
     }
 
+    function getChampionIconUrl(name) {
+        return `http://ddragon.leagueoflegends.com/cdn/10.15.1/img/champion/${name}.png`
+    }
+
+    // Fuction to format champ name for url -> Kai'sa = Kaisa / Jarvan IV = JarvanIV / Nunu & Willump = Nunu
+    // Function for creating the parts of the table which repeat
+
     const inn = [
+        el('tr', [
+            el('th', `${stateFirstTeam === 'Fail' ? 'Defeat' : 'Victory'}`, { className: stateFirstTeam}),
+            el('th', '', { className: stateFirstTeam}),
+            el('th', ''),
+            el('th', `${stateSecondTeam === 'Fail' ? 'Defeat' : 'Victory'}`, { className: stateSecondTeam}),
+            el('th', ``, { className: stateSecondTeam}),
+        ]),
         el('tr', [
             el('th', 'Player Names'),
             el('th', 'Champ Names'),
@@ -64,65 +81,95 @@ function createTable(names, champNames, profileIconIds) {
                 el('img', '', { src: getIconUrl(profileIconIds[0]), className: 'playerIcon'}),
                 names[0]
             ]),
-            el('td', champNames[0]),
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[0].split(' ').join('')), className: 'playerIcon'}),
+                champNames[0]
+            ]),
             el('td', ' '),
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[5]), className: 'playerIcon'}),
                 names[5]
             ]),
-            el('td', champNames[5])
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[5].split(' ').join('')), className: 'playerIcon'}),
+                champNames[5]
+            ]),
         ]),
         el('tr', [
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[1]), className: 'playerIcon'}),
                 names[1]
             ]),
-            el('td', champNames[1]),
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[1].split(' ').join('')), className: 'playerIcon'}),
+                champNames[1]
+            ]),
             el('td', ' '),
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[6]), className: 'playerIcon'}),
                 names[6]
             ]),
-            el('td', champNames[6])
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[6].split(' ').join('')), className: 'playerIcon'}),
+                champNames[6]
+            ]),
         ]),
         el('tr', [
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[2]), className: 'playerIcon'}),
                 names[2]
             ]),
-            el('td', champNames[2]),
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[2].split(' ').join('')), className: 'playerIcon'}),
+                champNames[2]
+            ]),
             el('td', ' '),
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[7]), className: 'playerIcon'}),
                 names[7]
             ]),
-            el('td', champNames[7])
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[7].split(' ').join('')), className: 'playerIcon'}),
+                champNames[7]
+            ]),
         ]),
         el('tr', [
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[3]), className: 'playerIcon'}),
                 names[3]
             ]),
-            el('td', champNames[3]),
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[3].split(' ').join('')), className: 'playerIcon'}),
+                champNames[3]
+            ]),
             el('td', ' '),
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[8]), className: 'playerIcon'}),
                 names[8]
             ]),
-            el('td', champNames[8])
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[8].split(' ').join('')), className: 'playerIcon'}),
+                champNames[8]
+            ]),
         ]),
         el('tr', [
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[4]), className: 'playerIcon'}),
                 names[4]
             ]),
-            el('td', champNames[4]),
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[4].split(' ').join('')), className: 'playerIcon'}),
+                champNames[4]
+            ]),
             el('td', ' '),
             el('td', [
                 el('img', '', { src: getIconUrl(profileIconIds[9]), className: 'playerIcon'}),
                 names[9]
             ]),
-            el('td', champNames[9])
+            el('td', [
+                el('img', '', { src: getChampionIconUrl(champNames[9].split(' ').join('')), className: 'playerIcon'}),
+                champNames[9]
+            ]),
         ])];
     const table = el('table', inn);
 
@@ -131,6 +178,9 @@ function createTable(names, champNames, profileIconIds) {
 
 async function getMatchData(gameId) {
         const match = await getMatchById(gameId);
+
+        const firstTeamState = match.teams[0].win;
+        const secondTeamState = match.teams[1].win;
 
         const participants = match.participants;
         participants.sort(p => p.participantId);
@@ -174,7 +224,9 @@ async function getMatchData(gameId) {
         return {
             names,
             champNames,
-            profileIconIds
+            profileIconIds,
+            firstTeamState,
+            secondTeamState
         }
 }
 
--- js/data.js
+++ js/data.js
@@ -1,22 +1,43 @@
 const api = 'insert dev api here';
-
+let ddragonVersion = '';
 
 export async function getUserBySummonerName(name) {
+    validateVersion();
     return (await fetch(`https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${api}`)).json();
 }
 
 export async function getIconById(id) {
-    return (await fetch(`http://ddragon.leagueoflegends.com/cdn/9.3.1/img/profileicon/27.png`));
+    validateVersion();
+    return (await fetch(`http://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/img/profileicon/27.png`));
 }
 
 export async function getMatchById(id) {
+    validateVersion();
     return (await fetch(`https://eun1.api.riotgames.com/lol/match/v4/matches/${id}?api_key=${api}`)).json();
 }
 
 export async function getMatchesByEncAccId(id) {
+    validateVersion();
     return (await fetch(`https://eun1.api.riotgames.com/lol/match/v4/matchlists/by-account/${id}?api_key=${api}`)).json();
 }
 
 export async function getChampionById(id) {
-    return (await fetch(`http://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion.json`)).json();
+    validateVersion();
+    return (await fetch(`http://ddragon.leagueoflegends.com/cdn/${ddragonVersion}/data/en_US/champion.json`)).json();
+}
+
+export async function setMostRecentVersion() {
+    ddragonVersion = await getMostRecentDdragonVersion();
+}
+
+async function getMostRecentDdragonVersion() {
+    const allVersions = await (await fetch('https://ddragon.leagueoflegends.com/api/versions.json')).json();
+    const mostRecentVersion = allVersions[0];
+    return mostRecentVersion;
+}
+
+function validateVersion() {
+    if (ddragonVersion === '') {
+        setMostRecentVersion();
+    }
 }
